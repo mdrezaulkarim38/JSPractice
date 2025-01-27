498c5d4de41e55f3c35089bdd43f48a7e91c8ae2
Rezaul Karim <mdrezaulkarim31295@gmail.com>
2025-01-27 21:36:08 +0600

    now added

--- first-ng-app/src/app/app.component.ts
+++ first-ng-app/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, HomeComponent],
   template: `
-    <h1>Welcome to {{title}}!</h1>
-    <p>Hello World</p>
-
+    <app-header />
+    <app-home />
     <router-outlet />
   `,
   styles: [`
