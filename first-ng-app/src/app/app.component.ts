2ed60a14cc33a63dcf00f0d9a3b3af321600885c
Rezaul Karim <mdrezaulkarim31295@gmail.com>
2025-01-27 21:27:45 +0600

    create two component

--- first-ng-app/src/app/app.component.ts
+++ first-ng-app/src/app/app.component.ts
@@ -6,10 +6,15 @@ import { RouterOutlet } from '@angular/router';
   imports: [RouterOutlet],
   template: `
     <h1>Welcome to {{title}}!</h1>
+    <p>Hello World</p>
 
     <router-outlet />
   `,
-  styles: [],
+  styles: [`
+    p{
+      background-color: red;
+    }
+    `],
 })
 export class AppComponent {
   title = 'Shuvo Love Farhana';
