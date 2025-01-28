b800efc374bd7b69d1463adee4953f8d52b43ad9
Rezaul Karim <mdrezaulkarim31295@gmail.com>
2025-01-28 22:21:39 +0600

    api added

--- first-ng-app/src/app/services/todos.service.ts
+++ first-ng-app/src/app/services/todos.service.ts
@@ -1,10 +1,12 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { Todo } from '../model/todo.type';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodosService {
+  http = inject(HttpClient);
   todoItems: Array<Todo> = [
     {
       title: 'Groceries',
@@ -18,6 +20,10 @@ export class TodosService {
       userId: 1,
       completed: false
     }
-  ]
+  ];
+  getTodosFromApi(){
+    const url = `https://jsonplaceholder.typicode.com/todos`;
+    return this.http.get<Array<Todo>>(url);
+  }
   constructor() { }
 }
