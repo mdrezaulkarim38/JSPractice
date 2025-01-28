43a4ac34070be7bfdf90c1827690c84b069e30ff
Rezaul Karim <mdrezaulkarim31295@gmail.com>
2025-01-28 22:26:05 +0600

    api data is coming

--- first-ng-app/src/app/todos/todos.component.ts
+++ first-ng-app/src/app/todos/todos.component.ts
@@ -1,12 +1,13 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { TodosService } from '../services/todos.service';
 import { Todo } from '../model/todo.type';
+import { catchError } from 'rxjs';
 
 @Component({
   selector: 'app-todos',
   imports: [],
   templateUrl: './todos.component.html',
-  styleUrl: './todos.component.scss'
+  styleUrl: './todos.component.scss',
 })
 export class TodosComponent implements OnInit {
   todoService = inject(TodosService);
@@ -15,5 +16,16 @@ export class TodosComponent implements OnInit {
   ngOnInit(): void {
     // console.log(this.todoService.todoItems);
     // this.todoItems.set(this.todoService.todoItems);
+    this.todoService
+      .getTodosFromApi()
+      .pipe(
+        catchError((err) => {
+          console.log(err);
+          throw err;
+        })
+      )
+      .subscribe((todos) => {
+        this.todoItems.set(todos);
+      });
   }
 }
