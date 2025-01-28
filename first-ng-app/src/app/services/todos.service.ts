c49e7c050f75601f21a461379b84a0544b3ec418
Rezaul Karim <mdrezaulkarim31295@gmail.com>
2025-01-28 22:08:34 +0600

    todo print in console

--- first-ng-app/src/app/services/todos.service.ts
+++ first-ng-app/src/app/services/todos.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { Todo } from '../model/todo.type';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodosService {
-
+  todoItems: Array<Todo> = [
+    {
+      title: 'Groceries',
+      id: 0,
+      userId: 1,
+      completed: false
+    },
+    {
+      title: 'Car wash',
+      id: 1,
+      userId: 1,
+      completed: false
+    }
+  ]
   constructor() { }
 }
