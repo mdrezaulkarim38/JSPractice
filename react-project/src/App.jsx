e2c1cbf2c2733a11a4e2edd0a6ba301e653cff01
Rezaul Karim <mdrezaulkarim31295@gmail.com>
2025-02-04 22:11:52 +0600

    testing code

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,15 +1,36 @@
-import "./App.css";
-import Header from "./components/Header";
-import Login,{ Profile} from "./components/UserComponent";
-function App() {
+import { useState, useEffect } from "react";
+
+// eslint-disable-next-line react/prop-types
+const Card = ({ title }) => {
+  const [count,setCount] = useState(0);
+  const [hasLiked, setHasLiked] = useState(false);
+  useEffect(()=>{
+    console.log(`${title} has been liked: ${hasLiked}`);
+  });
   return (
-    <div>
-      <Header />
-      <h2>First component</h2>
-      <Login />
-      <Profile />
+    <div className="card">
+      <h2>{title}</h2>
+      <p>{count}</p>
+      <button onClick={()=>{
+        setHasLiked(!hasLiked)
+        setCount(count + 1);
+        }} >
+        {hasLiked ? 'Liked' : 'Like' }
+      </button>
     </div>
   );
 }
 
-export default App;
+const App = () => {
+  return (
+    <div className="card-container">
+      <div>
+        <Card title="Star Wars" rating={5} isCoo={true} />
+        <Card title="Avatar" />
+        <Card title="The Lion King" />
+      </div>
+    </div>
+  );
+};
+
+export default App
