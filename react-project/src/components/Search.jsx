789e873b4fd59704e08a18b0083951ac04b2fc50
Rezaul Karim <mdrezaulkarim31295@gmail.com>
2025-02-05 07:38:25 +0600

    set code is done

---
+++ react-project/src/components/Search.jsx
@@ -0,0 +1,18 @@
+const person = {
+    name: 'Bruce Wayne',
+    age: 36,
+    location: 'Gotham City'
+}
+
+const Search = ({ searchTerm, setSearchTerm}) => {
+  return (
+    <div className="search">
+        <div>
+            <img src="search.svg" alt="search" />
+            <input type="text" placeholder="Search through thousands of movies" value={searchTerm} onChange={(event)=> setSearchTerm(event.target.value)}/>
+        </div>
+    </div>
+  )
+}
+
+export default Search
