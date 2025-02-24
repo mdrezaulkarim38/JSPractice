b5180f3abcf764c5991007aef30547766c4c5f44
Rezaul Karim <mdrezaulkarim31295@gmail.com>
2025-02-24 21:22:45 +0600

    create method

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,14 @@
 function App() {
     let fruit = "Apple";
+    const handleFruit = () => {
+        fruit = "Banana"
+        console.log(fruit);
+    }
     return (
         <div>
             <h1>State in React Js</h1>
             <h1>{fruit}</h1>
-            <button></button>
+            <button onClick={handleFruit}>Change Fruit name</button>
         </div>
     );
 }
