6e063113160915533842d99a3c4baf2660e5f3ed
Rezaul Karim <mdrezaulkarim31295@gmail.com>
2026-02-12 11:02:47 +0600

    create file

--- angular_tutorial/src/app/app.routes.ts
+++ angular_tutorial/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { DataBinding } from './components/data-binding/data-binding';
+import { Variables } from './components/variables/variables';
+import { Signal } from './components/signal/signal';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'data-binding',
+        component: DataBinding
+    },
+    {
+        path: 'signal',
+        component: Signal
+    },
+    {
+        path: 'variables',
+        component: Variables
+    }
+];
